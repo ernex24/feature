697b56360efd00528fd9e3bf20e8f7f93c2305ab
ernex24 <ernex24@gmail.com>
2019-03-04 23:14:05 +0100

    Add files via upload

---
+++ feature/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import Feature from './components/Feature';
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Feature />
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ feature/src/components/Feature/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Feature extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hola soy un Feature</h1>
+      </div>
+    )
+  }
+}
